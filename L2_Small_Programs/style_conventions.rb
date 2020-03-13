7eccbac5bc7d641b996f8a01cd7c6ab8a7c69b81
Anne <anne1365@outlook.com>
2020-03-12 21:49:42 -0700

    Ruby style conventions and addition of calculator (solo attempt) file

---
+++ L2_Small_Programs/style_conventions.rb
@@ -0,0 +1,21 @@
+# RUBY STYLE CONVENTIONS
+# files_should_be_snake_cased.rb
+# variables_should_be_too = 37
+
+def same_with_methods
+  #things
+end
+
+CONSTANT = "constant"
+
+
+#SINGLE VS. MULTILINE
+[1,2,3].each do |X| 
+  #stuff
+end
+
+[1,2,3].each {|x| #things}
+
+class ClassesShouldBeCamelCased
+end
+
