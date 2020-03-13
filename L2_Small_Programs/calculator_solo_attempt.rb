0eac140158228cf6dce874f216e0edb7d6826e88
Anne <anne1365@outlook.com>
2020-03-13 10:45:24 -0700

    Calculator solo attempt

---
+++ L2_Small_Programs/calculator_solo_attempt.rb
@@ -0,0 +1,37 @@
+loop do 
+
+  puts "Please enter a number: "
+  num_one = gets().chomp()
+  
+  puts "Please enter another number: "
+  num_two = gets().chomp()
+
+  if num_one.to_i.to_s != num_one || num_two.to_i.to_s != num_two
+    puts "Error: only numeric characters may be used."
+    next
+  end
+  
+  puts "What type of operation would you like to perform? 
+        (add, subtract, multiply, divide, or exit)"
+  operation = gets().chomp()
+  
+  case operation
+  when "add"
+    puts num_one.to_i + num_two.to_i
+  when "subtract"
+    puts num_one.to_i - num_two.to_i
+  when "multiply"
+    puts num_one.to_i * num_two.to_i
+  when "divide"
+    puts num_one.to_i / num_two.to_i
+  when "exit"
+    puts "Thanks for using the calculator app!"
+    break
+  else
+    puts "Invalid operation - please select add, subtract, 
+          multiply, divide, or exit"
+  end
+end
+
+
+
