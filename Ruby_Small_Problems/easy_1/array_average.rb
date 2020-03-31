5c5dd6be6ebf2eb3b281f6df4c87a74830191fc1
Anne Jones <anne1365@outlook.com>
2020-03-31 14:49:14 -0700

    Small problems work 03.31.2020 pt 1

--- Ruby_Small_Problems/easy_1/array_average.rb
+++ Ruby_Small_Problems/easy_1/array_average.rb
@@ -6,20 +6,48 @@ The array will never be empty and the numbers will always
 be positive integers.
 
 UNDERSTANDING THE PROBLEM:
-  INPUTS  -->
-  OUTPUTS -->
+  INPUTS  --> array
+  OUTPUTS --> integer
   RULES:
-  MENTAL MODEL:
+    - array will never be empty
+    - array will always contain positive integers
+  MENTAL MODEL: 
+    Method will accept an array or positive integers. We will iterate through
+    the array, adding each element to a variable we set at beginning of method. After
+    iterating through whole array we will divide the sum by the length of the array.
 
 EXAMPLES / TEST CASES: 
-
+  puts average([1, 5, 87, 45, 8, 8]) == 25
+  puts average([9, 47, 23, 95, 16, 52]) == 40
+  
 DATA STRUCTURE: 
-  INPUTS  -->
-  OUTPUTS -->
+  INPUTS  --> array
+  OUTPUTS --> integer
 
 ALGORITHM / PSEUDOCODE:
   CASUAL:
+    - set variable x = 0
+    - iterate through array:
+      - for each element add the value to x
+    - divide x by the length of the array
 
   FORMAL:
+    START method(array a)
+      SET variable x = 0
+      FOR EACH element in a
+        x += a
+      END
+      x / length of a
+    END
 
 =end
+
+def average(array)
+  sum = 0
+  array.each { |x| sum += x }
+  sum/ array.length #--------------- ------> FURTHER EXPLORATION - 'sum.to_f' would return avg as float
+end
+
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
+   
--- Ruby_Small_Problems/easy_1/sum_of_digits.rb
+++ Ruby_Small_Problems/easy_1/sum_of_digits.rb
@@ -1,22 +1,48 @@
 =begin
-PROBLEM: Write a method that takes one argument, a positive
+PROBLEM: 
+Write a method that takes one argument, a positive
 integer, and returns the sum of its digits.
 
 UNDERSTANDING THE PROBLEM:
-  INPUTS  -->
-  OUTPUTS -->
+  INPUTS  --> integer
+  OUTPUTS --> integer
   RULES:
+    - integer must be positive
+    - return value must be an integer
   MENTAL MODEL:
+  Our method will convert the integer input into a string, split the
+  string into chars, convert each char back into an integer, and sum these    
 
 EXAMPLES / TEST CASES: 
+  puts sum(23) == 5
+  puts sum(496) == 19
+  puts sum(123_456_789) == 45
 
 DATA STRUCTURE: 
-  INPUTS  -->
-  OUTPUTS -->
+  INPUTS  --> integer --> string --> array
+  OUTPUTS --> integer
 
 ALGORITHM / PSEUDOCODE:
-  CASUAL:
+  CASUAL: 
+    - convert input to string
+    - split string into array of chars
+    - convert chars into integers
+    - sum array
 
   FORMAL:
+    START method (integer x)
+      x to string
+      x to chars
+      FOR EACH char {convert to integer}
+      sum array
+    END
 
 =end
+
+def sum(num)
+  a = num.to_s.chars.map(&:to_i).sum
+end
+  
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
