37410713aa9e472bfa3560253a0242b32e1334c6
Anne Jones <anne1365@outlook.com>
2020-04-07 21:34:44 -0700

    L4 work

--- L4_Ruby_Collections/methods.rb
+++ L4_Ruby_Collections/methods.rb
@@ -1,39 +1,66 @@
-### EACH METHOD ####
-numbers = [1, 2, 3]
-counter = 0
+# ### EACH METHOD ####
+# numbers = [1, 2, 3]
+# counter = 0
 
-loop do 
-  break if counter == numbers. size
-  puts numbers[counter]
-  counter += 1
-end
+# loop do 
+#   break if counter == numbers. size
+#   puts numbers[counter]
+#   counter += 1
+# end
 
-# ^^ FUNCTIONALLY EQUIVALENT TO:
+# # ^^ FUNCTIONALLY EQUIVALENT TO:
 
-[1, 2, 3].each do |num|
-  puts num
-end
-
-
-hash = { a: 1, b: 2, c: 3}
-
-hash.each do |key, value| 
-  puts "Key: #{key} | Value: #{value}"
-end
+# [1, 2, 3].each do |num|
+#   puts num
+# end
 
 
-def a_method
-  [1, 2, 3].each do |num|
-    puts num * 2
+# hash = { a: 1, b: 2, c: 3}
+
+# hash.each do |key, value| 
+#   puts "Key: #{key} | Value: #{value}"
+# end
+
+
+# def a_method
+#   [1, 2, 3].each do |num|
+#     puts num * 2
+#   end
+# end
+# # 2, 4, 6 are outputted but original collection is returned
+
+
+
+# #### SELECT METHOD ####
+# p  [1, 2, 3].select do |num|
+#   num.odd?
+#  puts num
+#  end
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+c = [1, 2, 3]
+
+ d = a.map do |num|
+    'hi'
   end
-end
-# 2, 4, 6 are outputted but original collection is returned
+
+  p d
+  p a
 
 
 
-#### SELECT METHOD ####
-p  [1, 2, 3].select do |num|
-  num.odd?
- puts num
- end
+
+
+# p b.select do |num|
+#   num * 2
+#   puts num
+
+# end
+
+# p c.map do |num|
+#   num * 2
+#   puts num
+
+# end
 
