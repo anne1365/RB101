e86e8b5f0ecabf881990c51e02d3beaedd785d0b
Anne <anne1365@outlook.com>
2020-05-04 19:17:10 -0700

    Tic-tac-toe progress

--- L6_Slightly_Larger_Programs/Tic_Tac_Toe.rb
+++ L6_Slightly_Larger_Programs/Tic_Tac_Toe.rb
@@ -12,14 +12,21 @@ TTT Flow
   10. Goodbye!
 =end
 
+#CONSTANTS
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+#METHODS
 def prompt(msg)
   puts "=> #{msg}"
 end
 
 def display_board(brd)
+  system 'cls'
   puts ""
   puts "     |     |"
-  puts " #{brd[1]}   |  #{brd[2]}  |  #{brd[3]}"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
   puts "-----------------"
   puts "     |     |"
@@ -34,23 +41,81 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
-def player_places_piece(brd)
-  prompt "Choose a square (1-9): "
-  square = gets.chomp.to_i
-  
-  brd[square] = 'X'
+def empty_squares(brd)
+  brd.keys.select { |n| brd[n] == INITIAL_MARKER }
 end
 
+def player_places_piece(brd) 
+square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}): "
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "ERROR: That's not a valid choice."
+  end
+  
+  brd[square] = PLAYER_MARKER
+end
 
-board = initialize_board
-display_board(board)
+def computer_places_piece(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
 
-player_places_piece(board)
-puts board.inspect
-display_board(board)
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
 
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
 
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + #rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + #cols
+                  [[1, 5, 9], [3, 5, 7]]              #diags
+
+  winning_lines.each do  |line| 
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+       brd[line[1]] == COMPUTER_MARKER &&
+       brd[line[2]] == COMPUTER_MARKER
+       return "Computer"
+    end
+  end
+  nil
+end
+
+#MAIN GAME LOGIC
+loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt("Would you like to play again? (y/n)")
+  answer = gets.chomp.downcase
+  break if answer == 'n'
+end
