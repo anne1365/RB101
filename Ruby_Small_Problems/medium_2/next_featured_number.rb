ff5425722c9b8bce5e78033a780d8135c8943495
Anne Jones <anne1365@outlook.com>
2020-09-19 03:08:24 -0700

    Sync

---
+++ Ruby_Small_Problems/medium_2/next_featured_number.rb
@@ -0,0 +1,28 @@
+=begin ----------------------------------------------------------------------------------------------------------
+PROBLEM: given an integer argument, return the next featured number greater than the argument, or an error if
+         there is no next featured number
+
+UNDERSTANDING THE PROBLEM:
+  INPUTS  --> integer
+  OUTPUTS --> integer
+  
+  QUESTIONS:
+    - What's a featured number?
+        --> a odd number divisible by seven with no repeating digits
+    - Is there actually a limit to high these numbers can get? number can be no larger than 9,999,999,999
+  
+  RULES:
+    EXPLICIT:
+      - 
+    IMPLICIT:
+      - input number can be no larger than 9,999,999,999
+
+  IDEAS:
+
+DATA STRUCTURE:
+
+ALGORITHM:
+
+PSEUDOCODE:
+
+=end # ----------------------------------------------------------------------------------------------------------
